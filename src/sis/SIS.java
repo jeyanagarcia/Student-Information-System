c5e2c752d0083ddb1fcf3c6933827e648d21e44e
Jay Ann Garcia <92573753+jeyanagarcia@users.noreply.github.com>
2021-11-22 12:15:19 +0800

    initial commit

---
+++ src/sis/SIS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sis;
+
+import menuForm.menu;
+
+/**
+ *
+ * @author Reyn
+ */
+public class SIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        menu Mainmenu = new menu();
+        Mainmenu.show();
+    }
+    
+}
